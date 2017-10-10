f21777b80a926d58b7a3e1fd3623a4b514aaffea
ccpg_it <never1750@163.com>
2017-10-10 11:29:24 +0800

    初始化项目

---
+++ ZBHUDTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBHUDTools
+//
+//  Created by ccpg_it on 2017/10/10.
+//  Copyright © 2017年 never. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZBHUDTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZBHUDTools
+//
+//  Created by ccpg_it on 2017/10/10.
+//  Copyright © 2017年 never. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
